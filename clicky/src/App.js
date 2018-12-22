41175df02e48169eb234330aed10f309aaaed2bf
Shardayel17 <Shardayel17@yahoo.com>
2018-12-22 12:53:44 -0500

    json fixed

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -6,7 +6,7 @@ import Display from "./Components/Display";
 import Wrapper from "./Components/Wrapper";
 
 // to get card info from json file
-import cards from "./cards.json";
+import cards from "./card.json";
 
 
 
@@ -19,112 +19,184 @@ class App extends Component {
 state = {
   cards,
   score: 0,
-  correctIncorrect: "",
   clicked: [],
 
 };
 
 
 
-HandleClick = id => {
-  if (this.state.clicked.indexOf(id) === -1) {
-    this.handleIncrement();
-    this.setState({ clicked: this.state.clicked.concat(id) });
-  } else {
-    this.handleReset();
+
+
+
+CardClick = id => {
+  let guess = false;
+  console.log(id);
+  const newArray = this.state.chracters.map(character => {
+    const newCharacter = { ...character }
+    if (newCharacter.id === id && !newCharacter.clicked) {
+      console.log(character);
+      newCharacter.clicked = true
+      guess = true;
+    }
+    return newCharacter
+  })
+  if (guess){
+    this.winner(newArray);
 
   }
-};
-
-
-
-handleShuffle = () => {
-  let shuffledcards = randomcards(cards);
-  this.setState({ cards: shuffledcards });
-};
-
-
-
-handleIncrement = () => {
-  this.setState({ score: this.state.score + 1, correctIncorrect: "Nice!" });
-  if (this.state.score === 4) {
-    this.setState({ correctIncorrect: "You win!"});
+  else{
+    this.loser();
   }
+  
+}
 
-  this.handleShuffle();
-};
+winner = (newArray) => {
+  this.setState({ 
+    score: this.state.score + 1 ,
+    characters: this.shufflepic(newArray)
+  })
+  
+}
 
+loser = () => {
+  this.setState({ score: 0, chracters: [...cards], })
+}
 
-
-handleReset = () => {
+shufflepic = (data) => {
   this.setState({
-    score: 0,
-    correctIncorrect: "Correct!",
-    clicked: []
-  });
-
-  this.handleShuffle();
-
-};
-
-
+    chracters: data.sort(function (a, b) {
+      return 0.5 - Math.random();
+    })
+  })
 
 
 
 render() {
+
   return (
+
     <Wrapper>
-      <title> Guessing Game: {this.state.score}, { this.state.correctIncorrect} </title>
-<p> Earn 4 points to win</p>
-
-{/* //info to be identified in json file// */}
-
-{this.state.cards.map(card => (
-  <PictureCard 
-    id={Card.id}
-    key={Card.id}
-    image={Card.image}
-    name={Card.name}
-    
-    
-    
-    
-    
-    handleClick={this.handleClick}
-    handleIncrement={this.handleIncrement}
-    handleReset={this.handleReset}
-    handleShuffle={this.handleShuffle}
-  />
-))}
-</Wrapper>
-    );
-  }
+      <Display>Clicky
+        <br></br> Earn 4 pts and win <br></br>Score: {this.state.score}</Display>
+      {this.state.chracters.map(card => (
+        <PictureCard
+          CardClick={this.CardClick}
+          id={card.id}
+          key={card.id}
+          name={card.name}
+          image={card.image}
+        occupation={card.occupation}
+        location={card.location}
+        />
+      ))}
+    </Wrapper>
+  );
 }
 
 
-// import logo from './logo.svg';
-// import './App.css';
 
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Edit <code>src/App.js</code> and save to reload.
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//           </a>
-//         </header>
-//       </div>
+
+
+// HandleClick = id => {
+//   if (this.state.clicked.indexOf(id) === -1) {
+//     this.handleIncrement();
+//     this.setState({ clicked: this.state.clicked.concat(id) });
+//   } else {
+//     this.handleReset();
+
+//   }
+// };
+
+
+
+// handleShuffle = () => {
+//   let shuffledcards = randomcards(cards);
+//   this.setState({ cards: shuffledcards });
+// };
+
+
+
+// handleIncrement = () => {
+//   this.setState({ score: this.state.score + 1, correctIncorrect: "Nice!" });
+//   if (this.state.score === 4) {
+//     this.setState({ correctIncorrect: "You win!"});
+//   }
+
+//   this.handleShuffle();
+// };
+
+
+
+// handleReset = () => {
+//   this.setState({
+//     score: 0,
+//     correctIncorrect: "Correct!",
+//     clicked: []
+//   });
+
+//   this.handleShuffle();
+
+// };
+
+
+
+
+
+// render() {
+//   return (
+//     <Wrapper>
+//       <title> Guessing Game: {this.state.score}, { this.state.correctIncorrect} </title>
+// <p> Earn 4 points to win</p>
+
+// {/* //info to be identified in json file// */}
+
+// {this.state.cards.map(card => (
+//   <PictureCard 
+//     id={Card.id}
+//     key={Card.id}
+//     image={Card.image}
+//     name={Card.name}
+    
+    
+    
+    
+    
+//     handleClick={this.handleClick}
+//     handleIncrement={this.handleIncrement}
+//     handleReset={this.handleReset}
+//     handleShuffle={this.handleShuffle}
+//   />
+// ))}
+// </Wrapper>
 //     );
 //   }
 // }
 
+
+// // import logo from './logo.svg';
+// // import './App.css';
+
+// //   render() {
+// //     return (
+// //       <div className="App">
+// //         <header className="App-header">
+// //           <img src={logo} className="App-logo" alt="logo" />
+// //           <p>
+// //             Edit <code>src/App.js</code> and save to reload.
+// //           </p>
+// //           <a
+// //             className="App-link"
+// //             href="https://reactjs.org"
+// //             target="_blank"
+// //             rel="noopener noreferrer"
+// //           >
+// //             Learn React
+// //           </a>
+// //         </header>
+// //       </div>
+// //     );
+// //   }
+// }
+}
+}
 export default App;
